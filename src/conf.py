1290b2073e6059d0e0a91f10aa0cf044cefb3d5c
Raphael Hungria <rhja93@gmail.com>
2021-04-23 01:01:34 -0300

    read groups for dirty sync http responses

--- src/conf.py
+++ src/conf.py
@@ -1,5 +1,6 @@
 import pathlib
 import os
+import uuid
 
 import redis
 import dotenv
@@ -20,3 +21,13 @@ REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 SEASONS = ['winter', 'spring', 'summer', 'fall']
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+
+UUID = uuid.uuid4().hex
+GROUP_NAME = 'GATEWAY_API'
+RECOMMENDATION_RESULT_STREAM = f"GATEWAY_RECOMMENDATION_RESULT:{UUID}"
+
+try:
+    redis_client.xgroup_create(RECOMMENDATION_RESULT_STREAM, GROUP_NAME, mkstream=True)
+except redis.ResponseError as e:
+    if 'BUSYGROUP' not in str(e):
+        raise e
--- src/controllers/recommendation_controllers.py
+++ src/controllers/recommendation_controllers.py
@@ -5,20 +5,16 @@ from src import conf
 from src import util
 
 
-def build_rec_message(username, year, season):
+def build_model_request_msg(username, year, season):
     if season not in conf.SEASONS:
         raise util.WebServerException('invalid season', 400)
     
     try:
         request_message = {
-            'type': 'recommendation_request',
+            'type': 'create_model',
             'msg': json.dumps({
                 'timestamp': datetime.datetime.now().timestamp(),
                 'username': str(username),
-                'recommendation': {
-                    'year': int(year),
-                    'season': season
-                }
             })
         }
         return request_message
@@ -26,11 +22,34 @@ def build_rec_message(username, year, season):
         raise util.WebServerException('invalid entry', 400)
 
 
-def search_cache_for_recommendation(username, year, season):
-    res = conf.redis_client.get(f"recommendation:{username}:{year}:{season}")
+def build_recommendation_request_msg(username, anime_id):
+    try:
+        return {
+            'type': 'get_recommendation',
+            'msg': json.dumps({
+                'timestamp': datetime.datetime.now().timestamp(),
+                'uuid': conf.UUID,
+                'username': username,
+                'anime_id': int(anime_id),
+            })
+        }
+    except ValueError:
+        raise util.WebServerException('invalid body', 400)
 
 
-def create_recommendation_request(username, year, season):
-    msg = build_rec_message(username, int(year), season)
-    msg_id = conf.redis_client.xadd('recommendation_request', msg)
+def create_model_request(username, year, season):
+    msg = build_model_request_msg(username, int(year), season)
+    msg_id = conf.redis_client.xadd('create_model', msg)
+
+
+def get_recomendation_request(username, anime_id):
+    msg = build_recommendation_request_msg(username, anime_id)
+    conf.redis_client.xadd('get_recommendation', msg)
+    res = conf.redis_client.xreadgroup(
+        groupname=conf.GROUP_NAME,
+        consumername=conf.uuid,
+        streams={conf.RECOMMENDATION_RESULT_STREAM: '>'},
+        count=0    
+    )
+    return util.parse_message(res)
 
